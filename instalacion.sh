9566a8c01ecc5be761f2b3523c939abea92579a1
JP <jcargue0501@g.educaand.es>
2025-11-07 14:16:27 +0100

    Add superuser check before installing squid-proxy

--- instalacion.sh
+++ instalacion.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Este script instala squid-proxy
 
-apt update
-
-apt install squid-proxy
+if [[ $UID != 0 ]]
+then
+  echo "Entra como superusuario!"
+  exit
+else
+  apt update
+  apt install squid-proxy
+fi
