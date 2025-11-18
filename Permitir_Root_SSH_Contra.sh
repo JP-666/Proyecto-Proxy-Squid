a3580ac1fa7d662eaf4faf99cbe7bbea6891c08c
JP <jcargue0501@g.educaand.es>
2025-11-18 08:52:54 +0100

    Add script to login as root via SSH

---
+++ Permitir_Root_SSH_Contra.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Esto nos permite iniciar sesion como superusuario en ssh con contraseña, por no tener que estart creando certificados ni nada.
+# Esto es MUY inseguro, no deberias hacer esto en un servidor en producion
+
+sed 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config | tee /etc/ssh/sshd_config
+# "sed 's/string/nuevostring/' archivo" devuelve el contenido de archivo "archivo" con "string" reemplazo por "nuevostring".
+# "tee /etc/ssh/sshd_config" hace que la salida del comando anterior a stdin se escriba en un archivo, esto es recomendable si se utiliza alguna herramienta de escalacion de privilegios como sudo, en este caso, al asumir que todo el script se esta ejecutando como root, no hace falta, pero aun asi, es una forma 'correcta' de hacerlo.
+
+systemctl restart sshd
+# Reiniciamos el servidor ssh
