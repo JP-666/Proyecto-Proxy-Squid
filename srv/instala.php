2aa453657ecc1d6981f7f3edd60810c3a5b468ab
JP <jcargue0501@g.educaand.es>
2025-12-08 12:36:55 +0100

    Document security vulnerability in instala.php
    Added a comment highlighting the security vulnerability in the shell_exec command.

--- srv/instala.php
+++ srv/instala.php
@@ -1,7 +1,7 @@
 <?php
-$paquete=$_POST["paquete"];
-$resultado=shell_exec("apk add " . $paquete);
-echo str_replace("\n","<br>",$resultado);
+  $paquete=$_POST["paquete"];
+  $resultado=shell_exec("apk add " . $paquete); // Esto es una vulnerabilidad enorme, pues si alguien pasa el nombre de paquete como "paquete; (cosa maliciosa)" puede ejecutar comandos arbitrarios.
+  echo str_replace("\n","<br>",$resultado);
 ?>
 
 <br>
