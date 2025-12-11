05d6920fe1ff02e637f6b719d09db0ce51c22331
JP <jcargue0501@g.educaand.es>
2025-12-11 16:53:02 +0100

    Add IPTABLES rules for new network interfaces
    Added IPTABLES rules for additional networks.

--- todo.sh
+++ todo.sh
@@ -85,9 +85,15 @@ read -p "Presiona intro para continuar"
 echo "Haciendo IPTABLES"
 # Configura IPTABLES
 iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+# Red prof
 iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j REDIRECT --to-port 3128
 iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 443 -j REDIRECT --to-port 3129
-
+# Red Al1
+iptables -t nat -A PREROUTING -i enp0s9 -p tcp --dport 80 -j REDIRECT --to-port 3128
+iptables -t nat -A PREROUTING -i enp0s9 -p tcp --dport 443 -j REDIRECT --to-port 3129
+# Red Al2
+iptables -t nat -A PREROUTING -i enp0s10 -p tcp --dport 80 -j REDIRECT --to-port 3128
+iptables -t nat -A PREROUTING -i enp0s10 -p tcp --dport 443 -j REDIRECT --to-port 3129
 
 read -p "Presiona intro para continuar"
 
