3d0828a4adc1c0ef361ed284db878c3606fdb510
JP <jcargue0501@g.educaand.es>
2025-12-11 17:33:03 +0100

    Añadir la pagina de los alumnos

---
+++ srv/alumnos/index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Router Alumnos</title>
+	</head>
+	<body>
+		<h1>Este router es de los alumnos</h1>
+		<p>Lo unico que puedes hacer aqui es <a href="certi.pem">descargar el certificado</a></p>
+	</body>
+</html>
