cb494daf302d989b09d2475a7cb18cfd9a891ab5
JP-666 <jcargue0501@g.educaand.es>
2025-12-15 09:06:14 +0100

    Arreglar scripts auxiliares, extras

---
+++ extras/copias.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo ""
+echo "A continuacion se preparara la creacion de copias de seguridad"
+echo "Ejecuta en la maquina cliente 'nc -lvp 4444 | bash'"
+echo ""
+echo ""
+echo ""
