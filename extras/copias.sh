10e80f76ef138abc84f635bad9d5f426545a70fa
JP-666 <jcargue0501@g.educaand.es>
2025-12-21 13:33:03 +0100

    Script copias

--- extras/copias.sh
+++ extras/copias.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+read -p "Donde esta la DB (10.0.0.1) ? " otrohost
+read -p "Usuario del sistema (root) ? " usuario
 
-echo ""
-echo "A continuacion se preparara la creacion de copias de seguridad"
-echo "Ejecuta en la maquina cliente 'nc -lvp 4444 | bash'"
-echo ""
-echo ""
-echo ""
+if [[ -z $otrohost ]]
+then
+	otrohost=10.0.0.1
+fi
+
+if [[ -z $usuario ]]
+then
+	usuario=root
+fi
+
+
+# Generamos la clave local
+ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
+
+# La copiamos
+ssh-copy-id $usuario@$otrohost
+
+# Creamos el cron
+
+(crontab -l 2>/dev/null; echo "0 0 * * * ssh $usuario@otrohost -c 'mysqldump -d baseradius' > $HOME/RADIUS$RANDOM") | crontab -
