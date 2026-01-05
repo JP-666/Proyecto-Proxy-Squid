8cb485815e4fc5a24cf3c4a4b0c4a5bc059ebd63
JP-666 <jcargue0501@g.educaand.es>
2026-01-05 18:49:24 +0100

    Hacerlo todo un solo script. Ocurre todo solo

--- main.sh
+++ main.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-ArchAProbarAUX=("backup.sh" "crear_cuentas_sql.sh" "gen_acl.sh" "gen_variables.sh" generar_cert.sh generar_dhcp.sh generar_nginx.sh generar_red.sh generar_squid.sh instalar.sh iptables.sh jmail.sh mail.sh nginx.sh pers2.sh personalizaciones.sh radius.sh rootssh.sh)
-
-
 echo
 echo "Se va a proceder a comprobar unos requisitos... Por favor, espera"
 echo
@@ -32,19 +29,6 @@ then
 else
 	echo "[+] coco.jpeg presente"
 fi
-
-echo "[+] Comprobando archivos ejecutables"
-for i in "${ArchAProbarAUX[@]}"
-do
-	if [[ -x "aux/$i" ]]
-	then
-		echo "[+] Existe y es ejecutable: $i"
-	else
-		echo "[!] No existe o no es ejecutable: $i"
-		error="$error \nError en $i (No existe o no es ejecutable)"
-	fi
-sleep 0.1
-done
 
 
 
@@ -77,63 +61,5 @@ echo "	10. Configurar el servidor DHCP"
 echo "	11. Configurar RADIUS"
 echo # SON TABULADORES, NO ESPACIOS, QUE SI NO, SE QUEDAN RARO.
 echo
-
-echo "Primero, este programa soporta configurar dinamicamente los servicios, ¿Quieres personalizar la instalacion? (Redes, filtros, interfaces de configuracion, DHCP...)"
-read -p '(S)i, claro, hazme preguntas, ->(N)o, hazlo a tu manera :D -> [S/(N)] ? ' conf
-	case $conf in
-		S | s)
-			echo
-			echo "Genial, ahora se ejecutara el script de personalizacion, ten cuidado con tus respuestas, podras salir a un shell antes de continuar cuando acabes de personalizar"
-			echo
-			echo "Se configuraran las interfaces de red y el servidor dhcp"
-			echo
-			source aux/personalizaciones.sh
-			echo
-			echo "Has completado la configuracion, ahora vamos con las opciones miscelaneas, ten en cuenta que algunas de estas borraran tus opciones anteriores, o crearan conflictos, si no sabes lo que estas haciendo, rechaza todo"
-			echo
-			source aux/pers2.sh
-			echo
-			if [[ $interfaz == "no" ]]
-			then
-				echo "En el script anterior has seleccionado **NO** instalar la interfaz de config. Esto significa que NO se instalara nginx ni php ni se configurara el sitio."
-			fi
-			echo
-			read -p '¿Quieres cambiar algo [S/(N)]? > ' editar
-			if [[ $editar == "S" ]]
-			then
-				while true
-				do
-					echo "Parece que todo esta listo, ahora voy a dejar que edites los archivos a tu medida, escribe el nombre del archivo o escribe \"salir\" para salir del bucle"
-					echo
-					ls cosas/*.custom
-					echo
-					read -p "Archivo o salir ? > " editar
-					if [[ $editar == "salir" ]]
-					then
-						break
-					fi
-					if [[ ! -f cosas/$editar ]]
-					then
-						echo
-						echo "Whoops, eso no ha funcionado (No existe el archivo)"
-						echo
-					else
-						nano cosas/$editar
-					fi
-				done
-			fi
-			echo
-			echo "Ahora se generara la config de squid (basada en tus respuestas)"
-			echo
-			source aux/generar_squid.sh
-			echo
-			read -p "Se ha finalizado la parte de personalizacion, presiona enter para continuar con la instalacion"
-			source root.sh
-		;;
-		*)
-			echo "Perfecto entonces, se instalara todo con las configuracion por defecto!"
-			echo "¡Buena suerte!"
-			source pordefecto.sh
-		;;
-	esac
+source root.sh
 
--- root.sh
+++ root.sh
@@ -1,109 +1,168 @@
 #!/bin/bash
-if [[ $UID != 0 ]]
-then
-	echo "Entra como root!"
-	exit
-fi
-
-if [[ ! -v profesores ]]
-then
-	echo
-	echo "Falta la variable profesores"
-	echo "	- Este error no es fatal"
-	echo "	- Se continua la ejecucion"
-	echo
-fi
-
-
-if [[ ! -v interfaz ]]
-then
-	echo
-	echo "Falta la variable interfaz"
-	echo "  - Este error no es fatal"
-	echo "  - Se continua la ejecucion"
-	echo
-fi
-
-
-
-# Si tenemos alguna variable que SEA necesaria si o si, la metemos aqui, si falla, ponemos "fallo" a "true", parece ser que bash no nos permite simplemente usar if (lo que sea) true
-
-if [[ fallo == "true" ]]
-then
-	echo "Han fallado algunas comprobaciones de variables, por favor, comprueba los mensajes de arriba"
-	fallo=false
-	exit
-fi
-
-
-read -p "Este script va a realizar cambios en tu sistema, asegurate detenidamente que todo esta correcto antes de continuar, sal del script (CTRL-C) para salir a la shell, presionar enter para continuar "
-echo
+if [[ $UID != 0 ]]; then echo "Entra como root!"; exit; fi
 echo "[1] Instalacion"
-echo
-source aux/instalar.sh
-echo
+DEBIAN_FRONTEND=noninteractive apt install -y -qq iptables squid-openssl iptables-persistent isc-dhcp-server nginx php-fpm openssh-server git freeradius freeradius-mysql mariadb-common mariadb-server php-mysql mysql-common mariadb-client mariadb-server
 echo "[2] IPTABLES"
-echo
-source aux/iptables.sh
-echo
-echo
-echo
-# Pregunta sobre root
+
+iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j REDIRECT --to-port 3128
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 443 -j REDIRECT --to-port 3129
+iptables -t nat -A PREROUTING -i enp0s9 -p tcp --dport 80 -j REDIRECT --to-port 3128
+iptables -t nat -A PREROUTING -i enp0s9 -p tcp --dport 443 -j REDIRECT --to-port 3129
+iptables -t nat -A PREROUTING -i enp0s10 -p tcp --dport 80 -j REDIRECT --to-port 3128
+iptables -t nat -A PREROUTING -i enp0s10 -p tcp --dport 443 -j REDIRECT --to-port 3129
+netfilter-persistent save
+
 echo "[3] SSH - Root"
-read -p 'Permitir conexiones SSH como superusuario con contraseña  -> [(S)/N] ? ' sup
-case $sup in
-	N | n)
-		echo "Continuando sin permitir login como root con contraseña..."
-	;;
-	*)
-		$SHELL aux/rootssh.sh # No tenemos que sourcearlo, no tiene ninguna variable ni nada
-	;;
-esac
+echo "Permitiendo loguearse como Root con contraseña por SSH"
+cat /usr/share/openssh/sshd_config | sed 's/\#PermitRootLogin prohibit-password/PermitRootLogin yes/g' > /etc/ssh/sshd_config
+echo "Reiniciando SSH..."
+systemctl restart sshd
 echo "[4] Forwarding"
-echo
 echo "net.ipv4.ip_forward=1" > /etc/sysctl.conf
 sysctl -p
-echo
-echo "--- Ahora se configurara el servicio DHCP, las interfaces de red y se copiaran los archivos ---"
-echo
 echo "[5] Copiando archivos de /srv/"
 cp -rvf srv / >&2
-echo
-echo "[6] Asignaciones de variables!"
-if [[ -f cosas/interfaces.custom ]]; then archint=cosas/interfaces.custom; echo "Interfaces personalizadas"; else archint=cosas/interfaces; echo "Interfaces por defecto"; fi
-if [[ -f cosas/dhcp.custom ]]; then archdhcp=cosas/dhcp.custom; echo "DHCP personalizado"; else archdhcp=cosas/interfaces; echo "dhcp por defecto"; fi
-if [[ -f cosas/squid.custom ]]; then archsquid=cosas/squid.custom; echo "Squid personalizado"; else archsquid=cosas/squid.conf; echo "Squid por defecto"; fi
-if [[ -f cosas/sitio.custom ]]; then archsitio=cosas/sitio.custom; echo "Config de nginx personalizado"; else archsitio=cosas/sitio; echo "Conf de nginx por defecto"; fi
+echo "Saltando [6]..."
 echo "[7] Conf. Red"
-cp $archint /etc/network/interfaces -rvf
+cp cosas/interfaces /etc/network/interfaces -rvf
 echo " - Reiniciando red"
 systemctl restart networking
 echo "[8] Copiando squid"
-if [[ -f cosas/acl ]]; then cp -rvf cosas/acl /etc/squid/acl.txt; fi
-cp $archsquid /etc/squid/squid.conf -rvf
+cp -rvf cosas/acl /etc/squid/acl.txt
+cp cosas/squid.conf /etc/squid/squid.conf -rvf
 echo "[9] Creando certificados"
 echo
-aux/generar_cert.sh
-echo
-echo " - Reiniciando squid"
+
+read -p "Pais ? (Corto, p.e: ES) ? > " pais
+read -p "Region ? (p.e: Andalucia) ? > " reg
+read -p "Institucion ? (p.e: Instituto) ? > " ins
+read -p "Organizacion ? (p.e: InstitutoXYZ) ? > " org
+read -p "CA ? (p.e: Mi certificado para lo que sea) ? > " extra
+
+if [[ -z $pais ]]
+then
+	pais=ES
+fi
+if [[ -z $reg ]]
+then
+	reg=Andalucia
+fi
+if [[ -z $ins ]]
+then
+	ins=instituto
+fi
+if [[ -z $org ]]
+then
+	org=squidinstituto
+fi
+if [[ -z $extra ]]
+then
+	extra="CERTIFICADO PARA SSL DE SQUID DEL INSTITUTO"
+fi
+
+mkdir -p /etc/squid/ssl_cert
+chmod 700 /etc/squid/ssl_cert
+openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -keyout /etc/squid/ssl_cert/squid_ca.key -out /etc/squid/ssl_cert/squid_ca.pem -sha256 -subj "/C=$pais/ST=$reg/L=$reg/O=$ins/OU=$org/CN=$extra"
+chown proxy:proxy /etc/squid/ssl_cert -Rvf
+mkdir -p /var/lib/squid/
+/usr/lib/squid/security_file_certgen -c -s /var/lib/squid/ssl_db -M 4MB
+chown -R proxy:proxy /var/lib/squid/ssl_db
+
+echo " - Reiniciando squid (Esto va a tardar un poco...)"
 systemctl restart squid
-echo
 echo "[10] Conf. dhcp"
-cp $archdhcp /etc/dhcp/dhcpd.conf -rvf
+cp cosas/dhcp /etc/dhcp/dhcpd.conf -rvf
 cp cosas/isc-default /etc/default/isc-dhcp-server -rvf
 systemctl restart isc-dhcp-server
-# A lo mejor lo de NGINX se podria mover fuera, a su propio script.
-if [[ ! $interfaz == "no" ]]
-then
-	source aux/nginx.sh
-else
-	echo "Se ha saltado la instalacion de NGINX, tal como has pedido"
-fi
+
+echo "[11] Conf. Nginx"
+rm -rvf /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+cp -rvf cosas/sitio /etc/nginx/sites-enabled/default
+cp -rvf cosas/sitio /etc/nginx/sites-available/default
+php_dir=$(ls -d /etc/php/[0-9].* 2>/dev/null | tail -n 1)
+versionphp=$(basename "$php_dir")
+cp -rvf /etc/squid/ssl_cert/squid_ca.pem /srv/certi.pem
+ln -sf /srv/certi.pem /srv/alumnos/certi.pem
+mysql < cosas/base_router.sql
+echo -n "Introduce la nueva contraseña. NO SALDRA EN LA TERMINAL > "
+read -s ncont
+conthash=$(php -r "echo password_hash('$ncont', PASSWORD_BCRYPT);")
+mysql -D router -e "UPDATE datoslogin SET contrahash = \"$conthash\" WHERE usuario = \"admin\";"
+echo "www-data ALL=(ALL) NOPASSWD: /sbin/reboot" >> /etc/sudoers
+systemctl restart nginx php$versionphp-fpm
 echo "[12] Haciendo RADIUS"
-aux/radius.sh
-aux/jmail.sh
-echo
+cp -rvf cosas/radius-sitio-default /etc/freeradius/3.0/sites-enabled/default
+cp -rvf cosas/radius-eap /etc/freeradius/3.0/mods-enabled/eap
+cp -rvf cosas/sqlconfradius /etc/freeradius/3.0/mods-enabled/sql
+mysql -e "CREATE DATABASE baseradius;"
+mysql -e "CREATE USER 'Fran' IDENTIFIED BY 'FranPassword';"
+mysql -e "GRANT ALL ON baseradius.* TO 'Fran';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -D baseradius < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sql
+for i in {1..5};
+do
+	PROF=prof$i
+	CONT=$RANDOM$RANDOM$RANDOM
+	echo "Agregando usuario $PROF con contraseña $CONT"
+	mysql -u Fran -pFranPassword -D baseradius -e "INSERT INTO radcheck (username, attribute, op, value) VALUES ('$PROF', 'Cleartext-Password', ':=', '$CONT');"
+done
+i=0
+for i in {1..50};
+do
+	ALM=alum$i
+	CONT=$RANDOM$RANDOM$RANDOM
+	echo "Agregando usuario $ALM con contraseña $CONT"
+	mysql -u Fran -pFranPassword -D baseradius -e "INSERT INTO radcheck (username, attribute, op, value) VALUES ('$ALM', 'Cleartext-Password', ':=', '$CONT');"
+done
+exit
+echo "Permitiendo copias de seguridad ahora..."
+echo "[mysqld]" > /etc/mysql/mariadb.conf.d/99-permitir-copias.cnf
+echo "bind-address            = 0.0.0.0" >> /etc/mysql/mariadb.conf.d/99-permitir-copias.cnf
+systemctl restart freeradius mariadb
+
+cd jmail/
+
+echo "[13] Instalando JMAIL"
+make instalar
+echo "[+] Enviar correo (enviarcorreo)"
+cp -rvf enviarcorreo.sh /usr/bin/enviarcorreo
+chmod -Rvf +x /usr/bin/enviarcorreo
+
+echo "[+] Enviar correo seguro (enviarcorreoseguro)"
+cp -rvf enviarcorreoseguro.py /usr/bin/enviarcorreoseguro
+chmod -Rvf +x /usr/bin/enviarcorreoseguro
+
+echo "[+] Leer correo (leercorreo)"
+cp -rvf leercorreo.sh /usr/bin/leercorreo
+chmod -Rvf +x /usr/bin/leercorreo
+
+echo "[+] Configuracion (jmail.conf)"
+mkdir -p /etc/jmail
+cp -rvf jmail.conf /etc/jmail/
+echo "Configurando reglas de sudo..."
+mkdir -p /etc/sudoers.d/
+echo "www-data ALL=(ALL) NOPASSWD: /usr/bin/leerweb.sh" >> /etc/sudoers.d/jmail
+chmod 440 /etc/sudoers.d/jmail -Rvf
+echo "Instalando JQ para leer adjuntos"
+DEBIAN_FRONTEND=noninteractive apt install jq -y -qq
+echo "Copiando PHPs y activando el include"
+cp -rvf leerweb.sh /usr/bin/
+cp -rvf enviarcorreo.php /srv/
+cp -rvf descargar.php /srv/
+cp -rvf leercorreo.php /srv/
+cp -rvf vercorreosusuario.php /srv/
+cp -rvf jmail.php /srv/
+cp -rvf adjunto.php /srv/
+cat bashrc >> $HOME/.bashrc
+echo "Copiando archivos..."
+cp alertas.sh /usr/bin/alertas
+chmod +x /usr/bin/alertas
+cp alertas.service /etc/systemd/system/
+systemctl reload-daemon
+systemctl enable --now alertas
+
+cd -
+
 echo
 echo "Asegurate de instalar tambien de ejecutar aux/backup.sh en al menos un cliente!"
-echo
-echo "Proceso finalizado"
