4d0c63800efd8e4b7e7043601d3a7ef9e2a3749a
JP-666 <jcargue0501@g.educaand.es>
2026-01-06 12:15:44 +0100

    Borrar un poco del script de iVentoy que sobraba

--- iventoy.sh
+++ iventoy.sh
@@ -2,11 +2,9 @@
 
 
 urlv="https://github.com/ventoy/PXE/releases/download/v1.0.21/iventoy-1.0.21-linux-free.tar.gz"
-urli="https://???????/mx.iso"
+urli="https://???????/mx.iso" # CAMBIAME CUANDO VAYAS A PRESENTAR
 
 
-echo "Ahora se empezara a descargar MX en segundo plano"
-wget $urli &
 
 wget $urlv -O iventoy.tar.gz
 
@@ -14,23 +12,15 @@ tar xvf iventoy.tar.gz
 
 cd iventoy-*
 
+# No se puede hacer nada por configurarlo. El usuario tiene que hacer todo.
 
-echo "{
-  \"dhcp_mode\": 1,
-  \"server_ip\": \"192.168.1.50\",
-  \"pxe_port\": 16000,
-  \"http_port\": 17000
-}" > data/config.json
-
-
-./iventoy start
+./iventoy.sh start
 
 read -p "Ahora ve a la direccion que se te indica arriba desde otro navegador. Configura el modo a \"external\". Cuando este listo, activa la red que se vaya a activar (10.0.0.1), cuando todo este, pulsa enter aqui"
 
 echo "next-server 10.0.0.1;" >> /etc/dhcp/dhcpd.conf
-echo "filename \"iventoy_loader_17000\";" >> /etc/dhcp/dhcpd.conf
+echo "filename \"iventoy_loader_16000\";" >> /etc/dhcp/dhcpd.conf # El 17000 no funciona, por mucho que lo configures.
 
 
 systemctl restart isc-dhcp-server
-
-cp ../mx.iso iso/
+wget $urli -O iso/mx.iso
